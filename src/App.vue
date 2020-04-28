d2c5c903f88753fda45cac4678bf10d3f0377d8b
Vũ Văn Quang <vuquangit@gmail.com>
2020-04-28 18:13:02 +0700

    Init project

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app" class="container">
+    <Header />
+    <router-view />
+  </div>
+</template>
+
+<script>
+import Header from "./pages/Header";
+
+export default {
+  name: "App",
+  components: {
+    Header
+  }
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+.container {
+  width: 100%;
+  height: 100%;
+}
+
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center; */
+  /* color: #2c3e50; */
+  /* margin-top: 60px; */
+}
+</style>
---
+++ src/components/About.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="about">
+    <h1>About me</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "About",
+  data() {
+    return {
+      msg: "About page"
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  font-weight: normal;
+}
+</style>
